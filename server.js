8ca06bb7c2d3270464fc73962caa148516a80412
Bantesh <bsharma@aquimo.com>
2022-02-06 11:36:04 +0530

    Add PUT todos/:id

--- server.js
+++ server.js
@@ -59,6 +59,32 @@ app.delete('/todos/:id', function (req, res) {
     }
 })
 
+// PUT
+
+app.put('/todos/:id', (req, res) => {
+    const todoId = parseInt(req.params.id);
+    var matchedTodo = _.findWhere(todos, {
+        id: todoId
+    });
+    if (!matchedTodo) {
+        res.status(404).send('Asking for invalid todo with id of ' + req.params.id);
+    }
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+})
+
 app.listen(PORT, function () {
     console.log('Server is running on port ' + PORT);
 })
