3fa091748a28e4afec56cbbd66855caf6b3d65fc
Bantesh <bsharma@aquimo.com>
2022-02-07 09:36:00 +0530

    Minor update

--- playGround/basic-sqlite-dabase.js
+++ playGround/basic-sqlite-dabase.js
@@ -20,7 +20,9 @@ var Todo = sequelize.define('todo', {
     }
 })
 
-sequelize.sync({ force: true }).then(function () {
+sequelize.sync(
+    // { force: true }
+).then(function () {
     console.log('Everything is synced');
 
 
